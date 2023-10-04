b382dc7382115432859aae843f74dc511cb5e414
Benjamin Rodriguez <b.esteban.r.v@gmail.com>
2023-10-03 23:41:45 -0300

    [ADD] Init and first advance

---
+++ Calculadora-iOS/Calculadora-iOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Calculadora-iOS
+//
+//  Created by Benjamín on 03-10-23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        
+        return true
+    }
+
+    // MARK: - Private methods
+    
+
+}
+
---
+++ Calculadora-iOS/Calculadora-iOS/Controller/Home/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Calculadora-iOS
+//
+//  Created by Benjamín on 03-10-23.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    // MARK: - Initialization
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
